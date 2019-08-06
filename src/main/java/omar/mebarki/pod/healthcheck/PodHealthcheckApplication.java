f882527045d855b64872b15dc78df85c7758220d
Omar MEBARKI <omar.mebarki@gmail.com>
2019-08-06 20:01:29 +0200

    Init Project

---
+++ src/main/java/omar/mebarki/pod/healthcheck/PodHealthcheckApplication.java
@@ -0,0 +1,13 @@
+package omar.mebarki.pod.healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodHealthcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodHealthcheckApplication.class, args);
+	}
+
+}
